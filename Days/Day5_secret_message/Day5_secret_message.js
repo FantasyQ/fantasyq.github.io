0e5d638e4e85171792fe0db89a7405bb8bbc63e9
朱柏翰 <hunter77613@hotmail.com>
2016-07-04 15:18:42 +0800

    Day 5

---
+++ Days/Day5_secret_message/Day5_secret_message.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    var scrollHeight = '';
+    $(window).scroll(function () {
+        scrollHeight = $(this).scrollTop();
+        console.log(scrollHeight);
+
+        if (scrollHeight > 575 && scrollHeight < 830) {
+            $('.bottom_container').fadeIn(500);
+        } else {
+            $('.bottom_container').fadeOut(500);
+        };
+    });
+
+
+
+});
