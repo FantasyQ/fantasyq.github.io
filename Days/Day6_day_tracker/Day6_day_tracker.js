f979be18aa62078aa62c03ea344b79a60f385e19
朱柏翰 <hunter77613@hotmail.com>
2016-07-08 15:27:50 +0800

    Day 6 add clock

--- Days/Day6_day_tracker/Day6_day_tracker.js
+++ Days/Day6_day_tracker/Day6_day_tracker.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    var d = new Date();
-    var day = d.getDay();
     var msg = ['Sunday',
                'Blue Monday',
                'Blue Tuesday',
@@ -10,11 +8,29 @@ $(document).ready(function () {
                'TGIF',
                'Party Saturday!'
               ];
-    var dYear = d.getFullYear();
-    var dMonth = d.getMonth()+1;
-    var dDate = d.getDate();
-    $('.weekBox_today').text(dYear + ' / ' + dMonth + ' / ' + dDate);
-    $('#' + 'day' + day).css('background-color','#f6a445');
-    $('.weekBox_text').text(msg[day]);
+
+    function curr_time() {
+        var d = new Date();
+        var day = d.getDay();
+        var dYear = d.getFullYear();
+        var dMonth = d.getMonth()+1;
+        var dDate = d.getDate();
+        var hour = d.getHours();
+        var min = d.getMinutes();
+        var sec = d.getSeconds();
+
+        $('.weekBox_today').html( '<p>' + dYear + ' / ' + dMonth + ' / ' + dDate + '</p>' + '<p>' + addZero(hour) + ':' + addZero(min) + ':' + addZero(sec));
+        $('#' + 'day' + day).css('background-color','#f6a445');
+        $('.weekBox_text').text(msg[day]);
+    };
+
+    function addZero(i) {
+        if (i < 10) {
+            i = '0' + i;
+        };
+        return i;
+    };
+
+    setInterval(curr_time, 500);
 
 });
