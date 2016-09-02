8432a64e89e91adceb8006f541b035951374c3c9
朱柏翰 <hunter77613@hotmail.com>
2016-09-02 17:13:08 +0800

    no message

---
+++ Days180/Days/Day11_a_one_page_faq/Day11_a_one_page_faq.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.answer').hide();
+
+    $('.question').click(function () {
+        var $answer = $(this).next('.answer');
+        if (true) {
+
+        } else {
+
+        }
+    });
+});
