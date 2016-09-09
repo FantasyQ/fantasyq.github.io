df46d1ea127dd421800f85d69dce0239cde9b2ce
朱柏翰 <hunter77613@hotmail.com>
2016-09-09 14:58:43 +0800

    Day 11

--- Days180/Days/Day11_a_one_page_faq/Day11_a_one_page_faq.js
+++ Days180/Days/Day11_a_one_page_faq/Day11_a_one_page_faq.js
@@ -3,10 +3,13 @@ $(function () {
 
     $('.question').click(function () {
         var $answer = $(this).next('.answer');
-        if (true) {
-
+        var $icon = $(this).children('i');
+        if ($answer.is(':hidden')) {
+            $answer.slideDown(500);
+            $icon.attr('class', 'fa fa-minus-circle');
         } else {
-
+            $answer.slideUp(300);
+            $icon.attr('class', 'fa fa-plus-circle');
         }
     });
 });
